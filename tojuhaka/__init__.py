08528981a54218776618b3a8f5e18add5f8a5ae3
Toni Haka-Risku <tojuhaka@gmail.com>
2012-04-01 00:01:31 +0300

    manifest.in fixed

--- tojuhaka/__init__.py
+++ tojuhaka/__init__.py
@@ -18,7 +18,7 @@ def main(global_config, **settings):
     """
     config = Configurator(root_factory=root_factory, settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_translation_dirs('locale/')
+    config.add_translation_dirs('tojuhaka:locale/')
     config.set_locale_negotiator(my_locale_negotiator)
     config.scan()
     return config.make_wsgi_app()
